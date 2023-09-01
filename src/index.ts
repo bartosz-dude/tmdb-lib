3f3043a9a2752698ca8f77e4a8172317b39e4049
craftbox <bartoszdudzinski18@gmail.com>
2023-09-01 22:20:50 +0200

    added implemented api endpoints to one export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export const HelloLib = "Hello Lib!"
+import { TMDBAccount } from "./api/v3/account";
+import { TMDBCredits } from "./api/v3/credits";
+import { TMDBNetworks } from "./api/v3/networks/index";
+import { TMDBPeopleLists } from "./api/v3/peopleLists";
+
+export const TMDB = {
+	account: TMDBAccount,
+	credits: TMDBCredits,
+	networks: TMDBNetworks,
+	peopleLists: TMDBPeopleLists
+}
