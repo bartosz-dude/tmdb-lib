21b57f19241b77d85cdb15ae055700f3379917ad
bartosz-dude <bartoszdudzinskidev@gmail.com>
2024-08-28 18:30:40 +0200

    updated credits' JSDocs and interfaces

--- src/api/v3/credits/details.ts
+++ src/api/v3/credits/details.ts
@@ -2,76 +2,164 @@ import TMDBFetcher, { Fetcher } from "../../../fetcher"
 import { URLPaths } from "../../../tmdb"
 import TMDBUrlParser from "../../../urlParser"
 
-interface Request {
-	credit_id: number
+/**
+ * @link https://developer.themoviedb.org/reference/credit-details
+ */
+export interface TMDBCreditsDetailsRequest {
+	credit_id: string
 }
 
 type PathParams = {
-	credit_id: number
+	credit_id: string
 }
 
 type QueryParams = null
 
-interface Response {
-	credit_type: string,
-	department: string,
-	job: string,
+/**
+ * @link https://developer.themoviedb.org/reference/credit-details
+ */
+export interface TMDBCreditsDetailsResponse {
+	credit_type: string
+	department: string
+	job: string
 	media: {
-		adult: boolean,
-		backdrop_path: string,
-		id: number,
-		original_language: string,
-		original_name: string,
-		overview: string,
-		poster_path: string,
-		media_type: string,
-		genre_ids: number[],
-		popularity: number,
-		first_air_date: string,
-		vote_average: number,
-		vote_count: number,
-		origin_country: string[],
-		character: string,
-		episodes: [],
+		/**
+		 * @default true
+		 */
+		adult: boolean
+		backdrop_path: string
+		/**
+		 * @type int
+		 * @default 0
+		 */
+		id: number
+		original_language: string
+		original_name: string
+		overview: string
+		poster_path: string
+		media_type: string
+		/**
+		 * @type int[]
+		 * @default 0
+		 */
+		genre_ids: number[]
+		/**
+		 * @type number
+		 * @default 0
+		 */
+		popularity: number
+		first_air_date: string
+		/**
+		 * @type number
+		 * @default 0
+		 */
+		vote_average: number
+		/**
+		 * @type int
+		 * @default 0
+		 */
+		vote_count: number
+		origin_country: string[]
+		character: string
+		episodes: []
 		seasons: {
-			air_date: string,
-			episode_count: number,
-			id: number,
-			name: string,
-			overview: string,
-			poster_path: string,
-			season_number: number,
+			air_date: string
+			/**
+			 * @type int
+			 * @default 0
+			 */
+			episode_count: number
+			/**
+			 * @type int
+			 * @default 0
+			 */
+			id: number
+			name: string
+			overview: string
+			poster_path: string
+			/**
+			 * @type int
+			 * @default 0
+			 */
+			season_number: number
+			/**
+			 * @type int
+			 * @default 0
+			 */
 			show_id: number
 		}[]
 	}
-	media_type: string,
-	id: string,
+	media_type: string
+	id: string
 	person: {
-		adult: boolean,
-		id: number,
-		name: string,
-		original_name: string,
-		media_type: string,
-		popularity: number,
-		gender: number,
-		known_for_department: string,
+		/**
+		 * @default true
+		 */
+		adult: boolean
+		/**
+		 * @type int
+		 * @default 0
+		 */
+		id: number
+		name: string
+		original_name: string
+		media_type: string
+		/**
+		 * @type number
+		 * @default 0
+		 */
+		popularity: number
+		/**
+		 * @type int
+		 * @default 0
+		 */
+		gender: number
+		known_for_department: string
 		profile_path: string
 	}
 }
 
-export function TMDBCreditsDetails(request: Request, fetcher: Fetcher): Promise<Response>
-export function TMDBCreditsDetails(request: Request, readAccessToken: string): Promise<Response>
+/**
+ * Get a movie or TV credit details by ID.
+ *
+ * @link https://developer.themoviedb.org/reference/credit-details
+ */
+export function TMDBCreditsDetails(
+	request: TMDBCreditsDetailsRequest,
+	fetcher: Fetcher,
+): Promise<TMDBCreditsDetailsResponse>
+/**
+ * Get a movie or TV credit details by ID.
+ *
+ * @link https://developer.themoviedb.org/reference/credit-details
+ */
+export function TMDBCreditsDetails(
+	request: TMDBCreditsDetailsRequest,
+	readAccessToken: string,
+): Promise<TMDBCreditsDetailsResponse>
 
-export default function TMDBCreditsDetails(request: Request, fetcherOrApi: Fetcher | string): Promise<Response> {
-	const url = TMDBUrlParser<PathParams, QueryParams>(URLPaths.CREDIT, "{credit_id}", {
-		path: {
-			credit_id: request.credit_id
-		}
-	})
+/**
+ * Get a movie or TV credit details by ID.
+ *
+ * @link https://developer.themoviedb.org/reference/credit-details
+ */
+export default function TMDBCreditsDetails(
+	request: TMDBCreditsDetailsRequest,
+	fetcherOrApi: Fetcher | string,
+): Promise<TMDBCreditsDetailsResponse> {
+	const url = TMDBUrlParser<PathParams, QueryParams>(
+		URLPaths.CREDIT,
+		"{credit_id}",
+		{
+			path: {
+				credit_id: request.credit_id,
+			},
+		},
+	)
 
 	if (typeof fetcherOrApi == "string") {
 		return TMDBFetcher(url, fetcherOrApi)
 	} else {
-		return fetcherOrApi<Response>(url)
+		return fetcherOrApi<TMDBCreditsDetailsResponse>(url)
 	}
 }
--- src/api/v3/credits/index.ts
+++ src/api/v3/credits/index.ts
@@ -1,5 +1,12 @@
-import { TMDBCreditsDetails } from "./details";
+import { TMDBCreditsDetails } from "./details"
 
-export const TMDBCredits = {
-	details: TMDBCreditsDetails
+const TMDBCredits = {
+	/**
+	 * Get a movie or TV credit details by ID.
+	 *
+	 * @link https://developer.themoviedb.org/reference/credit-details
+	 */
+	details: TMDBCreditsDetails,
 }
+
+export default TMDBCredits
